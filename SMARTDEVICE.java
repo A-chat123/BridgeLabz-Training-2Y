bee6b03ac97fa85b20182e423266ea71d6ce736d
Anish Bansal <bansalanish274@gmail.com>
2025-10-15 15:26:53 +0530

    OOP

---
+++ SMARTDEVICE.java
@@ -0,0 +1,23 @@
+public class SMARTDEVICE {
+  interface SmartDevice {
+    void operate(String deviceName);
+}
+
+
+    public static void main(String[] args) {
+        SmartDevice turnOn = name -> System.out.println(name + " is now ON");
+        SmartDevice turnOff = name -> System.out.println(name + " is now OFF");
+
+        turnOn.operate("Light");
+        turnOn.operate("AC");
+        turnOn.operate("TV");
+
+        System.out.println("---- Turning Off Devices ----");
+
+        turnOff.operate("Light");
+        turnOff.operate("AC");
+        turnOff.operate("TV");
+    }
+}
+
+
