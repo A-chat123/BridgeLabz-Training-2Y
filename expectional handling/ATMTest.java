c6572ea35530d8b209524cf34f87ef9fc3e5f70f
Anish Bansal <bansalanish274@gmail.com>
2025-11-10 13:46:09 +0530

    OOP

---
+++ expectional handling/ATMTest.java
@@ -0,0 +1,28 @@
+// 1. ATM Withdrawal System
+class InsufficientFundsException extends Exception {
+  public InsufficientFundsException(String message) {
+      super(message);
+  }
+}
+
+class ATM {
+  private double balance = 10000;
+
+  public void withdraw(int amount) throws InsufficientFundsException {
+      if (amount > balance)
+          throw new InsufficientFundsException("Insufficient balance! Available: ₹" + balance);
+      balance -= amount;
+      System.out.println("Withdrawal successful. Remaining balance: ₹" + balance);
+  }
+}
+
+public class ATMTest {
+  public static void main(String[] args) {
+      ATM atm = new ATM();
+      try {
+          atm.withdraw(15000);
+      } catch (InsufficientFundsException e) {
+          System.out.println(e.getMessage());
+      }
+  }
+}
